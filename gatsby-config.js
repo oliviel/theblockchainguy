5320c04b4374050acce3e28f4d143df753513e6b
oliviel <olivielultimate@gmail.com>
2021-11-02 12:10:49 -0400

    Added google track id to the head

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_TRACK_ID,
+        head: true,
       },
     },
     {
